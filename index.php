921f9bb4de13560a4ebc7a78308f370f141e7eed
Laura_M <l.martinez9388@gmail.com>
2021-12-15 19:14:11 +0100

    correctifs

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once('functions.php');
 
 <html>
 	<head>
-		<title>Tables de multiplication</title>
+		<title>TP1. PHP impératif</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
